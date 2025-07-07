1d0bf44e4847d64f28e0003b228c268832a929fa
geethasamynathan <geethasamynathan2011@gmail.com>
2025-07-07 16:38:50 +0530

    gitignore added

---
+++ Personal/Hexa may 25/cicd_solution/ProductAPIDemo/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPIDemo.Models;
+
+namespace ProductAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Hard-coded list for demo
+        private static readonly List<Product> _products =
+        [
+            new(1, "Keyboard", 49.99m),
+            new(2, "Mouse", 29.99m),
+            new(3, "Headset", 89.99m)
+        ];
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+            => Ok(_products);
+    }
+}
